7f56597f1d8ca9bad610004becd7a2d70234fcf6
Anawat Jarusiripot <anawat@teohong.com>
2021-07-21 12:54:00 +0700

    categories

---
+++ lib/dummy_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: "c1", title: "Italian", color: Colors.orange),
+  Category(id: "c2", title: "Quick And Easy", color: Colors.red),
+  Category(id: "c3", title: "Hambergers", color: Colors.amber),
+  Category(id: "c4", title: "German", color: Colors.amber),
+  Category(id: "c5", title: "Light and Lovely", color: Colors.amber),
+];
